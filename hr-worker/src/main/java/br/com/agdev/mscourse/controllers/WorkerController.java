52b9344043fd6f0f32afa4b325312bc4ee335ced
AlsGomes <als_08.net@hotmail.com>
2023-01-23 16:15:17 -0300

    Added Ribbon load balancer

--- hr-worker/src/main/java/br/com/agdev/mscourse/controllers/WorkerController.java
+++ hr-worker/src/main/java/br/com/agdev/mscourse/controllers/WorkerController.java
@@ -3,6 +3,7 @@ package br.com.agdev.mscourse.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,13 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.agdev.mscourse.entities.Worker;
 import br.com.agdev.mscourse.repositories.WorkerRepository;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/workers")
 public class WorkerController {
 
 	@Autowired
 	private WorkerRepository repository;
+
+	@Autowired
+	private Environment env;
 
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
@@ -27,6 +33,9 @@ public class WorkerController {
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+		log.info("PORT=" + env.getProperty("local.server.port"));
+
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
